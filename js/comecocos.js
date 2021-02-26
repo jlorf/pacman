14fe1907d783f0edc8014eee1239b3cf1cbf8d5c
Joel <jlorenzofresneda@gmail.com>
2021-02-26 16:16:10 +0000

    Doblar velocitat cada cop que agafa el powerup

--- js/comecocos.js
+++ js/comecocos.js
@@ -251,7 +251,7 @@ const s = p => {
               pacman.vides = pacman.vides + 1;
               break;
             case 1:
-              p.frameRate(parseInt(dificultat) * 2);
+              p.frameRate(p.frameRate() * 2);
               if (fpstimeout){
                 window.clearTimeout(fpstimeout);
               }
